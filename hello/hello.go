05d4cde531834bdda088b25e8ea0db7e4293baa2
daubejb <daubejb@gmail.com>
2020-08-26 16:19:41 -0400

    add README, make hello accept "" string for name

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 //Hello - yep
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
