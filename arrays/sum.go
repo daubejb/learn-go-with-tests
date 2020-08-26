81926cb9a8f28b70aadd9143f371df2111126ed9
daubejb <daubejb@gmail.com>
2020-08-26 17:58:27 -0400

    implement SumAll and test

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,4 +1,4 @@
-package sum
+package main
 
 // Sum takes an array of integers and returns their sum
 func Sum(numbers []int) int {
@@ -7,4 +7,16 @@ func Sum(numbers []int) int {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll calculates the respective sums of every slice passed in.
+func SumAll(numbersToSum ...[]int) []int {
+	numberOfGroups := len(numbersToSum)
+	sums := make([]int, numberOfGroups)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
