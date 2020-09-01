ea9d5c371f93b9c44ac7d3f82f0b0dc9b26a6a59
daubejb <daubejb@gmail.com>
2020-09-01 14:33:41 -0400

    write Area function

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -2,5 +2,10 @@ package perimeter
 
 // Perimeter - returns the perimeter given a width and height
 func Perimeter(width float64, height float64) float64 {
-	return 0
+	return 2 * (width + height)
+}
+
+// Area - returns the area given a width and a height
+func Area(width, height float64) float64 {
+	return width * height
 }
