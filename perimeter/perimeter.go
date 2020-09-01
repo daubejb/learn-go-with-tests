36b67f35d3d6e0f65cacbf34a056faf7db89ba4a
daubejb <daubejb@gmail.com>
2020-09-01 15:41:37 -0400

    modify functions to accept a rectangle struct

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,11 +1,17 @@
 package perimeter
 
+// Rectangle - represents a rectangle by specifying its width and height
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter - returns the perimeter given a width and height
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area - returns the area given a width and a height
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
