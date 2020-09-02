26880747696500349cadc0c4213f9db65ac47adb
daubejb <daubejb@gmail.com>
2020-09-02 17:12:50 -0400

    write fail test for Add

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -2,6 +2,21 @@ package dictionary
 
 import "testing"
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
