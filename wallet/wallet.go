ca0556a67c91ed9f2801c732bc9cb560ed594aab
daubejb <daubejb@gmail.com>
2020-09-02 14:42:17 -0400

    add pointers to wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,15 +1,19 @@
 package wallet
 
+import "fmt"
+
 // Wallet - contains a balance
 type Wallet struct {
+	balance int
 }
 
 // Deposit - puts money into a wallet
-func (w Wallet) Deposit(amount int) {
-
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
 }
 
 // Balance - retrieves the balance of a wallet
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Balance() int {
+	return w.balance
 }
