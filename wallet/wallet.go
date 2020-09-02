f6fee0a622d722afa57c6a73deb42054ee10a4e7
daubejb <daubejb@gmail.com>
2020-09-02 15:02:20 -0400

    refactor wallet test

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,6 +2,14 @@ package wallet
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Bitcoin int
 
 type Wallet struct {
@@ -9,8 +17,11 @@ type Wallet struct {
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
