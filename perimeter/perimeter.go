da876a40cc21a00d084cdb0144809f38349e92dd
daubejb <daubejb@gmail.com>
2020-09-02 09:19:45 -0400

    implement methods for circle and rectangle

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,9 +1,26 @@
 package perimeter
 
+import "math"
+
 // Rectangle - represents a rectangle by specifying its width and height
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// Area - returns the area of a rectangle given a Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle - a circle as represented by radius
+type Circle struct {
+	Radius float64
+}
+
+// Area - calculates the area of a cicle given a radius
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 // Perimeter - returns the perimeter given a width and height
