7f9474066de6f9147db037c64602644e14685dac
daubejb <daubejb@gmail.com>
2020-09-02 17:42:40 -0400

    implement error constants within DictionaryErr type implementing the error interface

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,13 +1,17 @@
 package dictionary
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("Could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound   = DictionaryErr("Could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Add(word, definition string) error {
 	_, err := d.Search(word)
